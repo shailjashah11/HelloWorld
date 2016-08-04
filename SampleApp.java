807d9a48f5826e96b37380a8bc0000c0617285fb
Shailja Shah <shailja.shah@MSBC-MAINFRAME.LCL>
2016-08-04 16:12:46 +0530

    commited

---
+++ SampleApp.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sampleapp;
+import com.bloomberglp.blpapi.Session;
+import qtech.EMSXBridge;
+import qtech.IEMSXBridge;
+import qtech.Main;
+/**
+ *
+ * @author shailja.shah
+ */
+public class SampleApp  {
+
+    /**
+     * @param args the command line arguments
+     * 
+     * 
+     */
+   
+    
+
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        // TODO code application logic here
+        System.out.println("Hello World!");         
+               
+        
+        
+    }
+    
+    
+}
